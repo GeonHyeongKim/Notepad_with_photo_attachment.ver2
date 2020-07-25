f3b80362e25761e28859eece9572415fb8022316
김건형 <show11837@gmail.com>
2020-07-26 03:50:16 +0900

    feat : iPad 지원 - 공유 기능 문제
    + popoverPresentationController

--- Notepad.ver2/DetailViewController.swift
+++ Notepad.ver2/DetailViewController.swift
@@ -61,10 +61,14 @@ class DetailViewController: UIViewController {
 
     }
     
-    @IBAction func share(_ sender: Any) {
+    @IBAction func share(_ sender: UIBarButtonItem) {
         guard let memo = memo?.content else { return }
         
         let vc = UIActivityViewController(activityItems: [memo], applicationActivities: nil)
+        
+        if let pc = popoverPresentationController {
+            pc.barButtonItem = sender
+        }
         
         present(vc, animated: true, completion: nil)
     }
