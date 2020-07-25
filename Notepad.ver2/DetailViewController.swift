4b9dfe9216804fba9cbdca04b81806d2bbe7d897
김건형 <show11837@gmail.com>
2020-07-26 02:57:38 +0900

    feat : 메모 공유
    + toolbar에 item 추가 : style item : action
    + button action : UIActivityViewController

--- Notepad.ver2/DetailViewController.swift
+++ Notepad.ver2/DetailViewController.swift
@@ -61,7 +61,14 @@ class DetailViewController: UIViewController {
 
     }
     
-
+    @IBAction func share(_ sender: Any) {
+        guard let memo = memo?.content else { return }
+        
+        let vc = UIActivityViewController(activityItems: [memo], applicationActivities: nil)
+        
+        present(vc, animated: true, completion: nil)
+    }
+    
     /*
     // MARK: - Navigation
 
