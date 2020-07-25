6c0b00905878c90d9173287849f768010d8ec6bd
김건형 <show11837@gmail.com>
2020-07-25 16:53:52 +0900

    feat : 데이터 모델화를 위한 model class
    + note class
    	content: String
    	insertDate: Date
    
    + dummy data 배열
            Note(content: "Feedback content"),
            Note(content: "TODO")

---
+++ Notepad.ver2/Model/Model.swift
@@ -0,0 +1,24 @@
+//
+//  Model.swift
+//  Notepad.ver2
+//
+//  Created by gunhyeong on 2020/07/25.
+//  Copyright © 2020 geonhyeong. All rights reserved.
+//
+
+import Foundation
+
+class Note {
+    var content: String
+    var insertDate: Date
+    
+    init(content: String) {
+        self.content = content
+        self.insertDate = Date()
+    }
+    
+    static var dummyNote = [
+        Note(content: "Feedback content"),
+        Note(content: "TODO")
+    ]
+}
